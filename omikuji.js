8689efe1394409a10d98241e03af957239a1241f
bjxdfm <85215052+bjxdfm@users.noreply.github.com>
2021-06-09 15:00:27 +0900

    Update omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -9,3 +9,26 @@ window.addEventListener("DOMContentLoaded",
         window.alert(popMessage);
     },false
 );
+
+const btn1=document.getElementById("btn1")
+btn1.addEventListener("click",
+    function(){
+        let n =Math.floor(Math.random()*3);
+
+        switch (n){
+            case 0:
+                btn1.textContent="大吉！!";
+                btn1.style.color="#ff0000";
+                break;
+            case 1:
+                btn1.textContent="吉！";
+                btn1.style.color="#fff001";
+                break;
+            case 2:
+                btn1.textContent="末吉...";
+                btn1.style.color="#261e1c";
+                break;    
+
+        }
+    },false
+);
