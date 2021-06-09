4f94514d27726770ba69bfb59b8b0567092e0f39
bjxdfm <85215052+bjxdfm@users.noreply.github.com>
2021-06-09 14:16:44 +0900

    Create omikuji.js

---
+++ omikuji.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded",
+    function(){
+        // 
+
+        //
+        let popMessage="いらっしゃい！おみくじ引いてって！";
+        window.alert(popMessage);
+    },false
+);
